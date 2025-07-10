87ca2f49c32ea6ab26c7a4df14375f046997e777
Viviana Gonzalez <vivigz06@gmail.com>
2025-07-10 02:11:28 -0300

    levanté el servidor y conecté  a la base de datos

---
+++ app.js
@@ -0,0 +1,21 @@
+ import express from 'express';
+ import dotenv from 'dotenv';
+ import sequelize from './src/config/database.js';
+//  import characterRoutes from './src/routes/character.routes.js';
+ dotenv.config();
+
+ const app = express();
+ app.use(express.json());
+
+//  app.use('/api/characters', characterRoutes);
+
+ const PORT = process.env.PORT || 4000;
+
+
+sequelize.sync().then(() => {
+ console.log('Base de datos conectada correctamente');
+ app.listen(PORT, () => {
+ console.log(`Servidor escuchando en http://localhost:${PORT}`);
+ });
+ });
+
